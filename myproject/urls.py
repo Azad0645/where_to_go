0c4c4944edac57ce8c2c5b0212fae01afd583db9
Azad0645 <fenix16450@gmail.com>
2025-10-03 15:12:50 +0300

    Поменял название приложения main

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from main.views  import index, place_json
+from places.views  import index, place_json
 from django.conf import settings
 from django.conf.urls.static import static
 
